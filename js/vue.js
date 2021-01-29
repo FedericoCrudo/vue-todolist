ea7bb94e36e435940ced99ca2b1aca874b13104a
Federico <crudofederico@gmail.com>
2021-01-29 16:23:49 +0100

    work in progress

--- js/vue.js
+++ js/vue.js
@@ -3,15 +3,20 @@
 // Poi aggiungete all'immagine una classe sempre presa dal data.
 $(document).ready(function(){
     setTimeout(function(){ $(".contenuto").addClass("animation"); }, 5000);
-  
+    $(".link").click(function () {
+        $(this).toggleClass("dark-botton");
+        $("body").toggleClass("bg_color_blue");
+    })
   });
 var print = new Vue({
     el: '.prova',
     data: {
     message:'',
+    itemsFilter:[],
    itemsDelete:[],
     items:[
         {
+            "index":0,
             "user":"Luca",
             "email":"luca@gmail.com",
             "password":"luca",
@@ -19,42 +24,99 @@ var print = new Vue({
             
         },
         {
+            "index":1,
             "user":"Luca",
             "email":"luca@gmail.com",
             "password":"luca",
             "stato":"off"
         },
         {
+            "index":2,
             "user":"licci",
             "email":"licci@gmail.com",
             "password":"liccu",
             "stato":"on"
         },
         {
+            "index":3,
             "user":"Lorenzo",
             "email":"lorenzo@gmail.com",
             "password":"lore",
             "stato":"off"
+        },
+        {
+            "index":4,
+            "user":"Marta",
+            "email":"lorenzo@gmail.com",
+            "password":"marta",
+            "stato":"off"
+        },
+        {
+            "index":5,
+            "user":"Federico",
+            "email":"federico@gmail.com",
+            "password":"federico",
+            "stato":"off"
+        },
+        {
+            "index":6,
+            "user":"Francesco",
+            "email":"francesco@gmail.com",
+            "password":"lore",
+            "stato":"off"
         }
     ]
-
     },
     methods:{
         remove(index){
         this.itemsDelete.push(this.items[index]);
         this.items.splice(index,1);
+        this.notifydelete();
         },
-        restore(index){
-            this.items.push(this.itemsDelete[index]);
+        restore(index,itemindex){
+            this.items.splice(itemindex,0,this.itemsDelete[index]);//cosi facendo l'array tornera alla posizione
             this.itemsDelete.splice(index,1);   
+            this.notifyrestore();
         },
         removeDelete(index){
             this.itemsDelete.splice(index,1);
+            this.notifydelete();
+        },
+        removeall(){
+            this.itemsDelete=[];
         },
         display(){
             (this.itemsDelete.length!=0)?$(".buttonDelete").css("display","inline-block"):$(".buttonDelete").css("display","none");
-            console.log(this.itemsDelete.length);
-        }
+        },
+        notifydelete(){
+            $("#notify").text("");
+            $(".windowsNotification").removeClass("notification-animation");
+            $("#notify").text("Elemento eliminato con successo");
+            $(".windowsNotification").addClass("notification-animation");
+            setTimeout(function(){ $(".windowsNotification").removeClass("notification-animation"); }, 1000);
+        },
+        notifyrestore(){
+            $("#notify").text("");
+            $(".windowsNotification").removeClass("notification-animation");
+            $("#notify").text("Elemento Ripristinato con Successo");
+            $(".windowsNotification").addClass("notification-animation");
+            setTimeout(function(){ $(".windowsNotification").removeClass("notification-animation"); }, 1000);
+        },
+         filtered() {
+           
+            if(this.message.length>=1){
+                this.itemsFilter=this.items.filter((element)=>{
+                    return element.user.includes(this.message);
+                    });  
+                    console.log(this.itemsFilter);     
+            }
+            (this.message.length==0)?this.itemsFilter=[]:"";
+           
+        },
+     
+
     }
+    
+  
     
     });
