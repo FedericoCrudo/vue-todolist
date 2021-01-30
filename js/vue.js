0553add519093d3bf8fdbf2b085dd7e32f423294
Federico <crudofederico@gmail.com>
2021-01-31 00:37:33 +0100

    update

--- js/vue.js
+++ js/vue.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
         $(this).toggleClass("dark-botton");
         $("body").toggleClass("bg_color_red");
         $(".container").toggleClass("bg_color_container");
+        $(".btn1").click(function(){
+            $("p").slideUp();
+          });
     })
   });
 var print = new Vue({
@@ -38,7 +41,7 @@ var print = new Vue({
         },
         {
             "index":2,
-            "user":"licci",
+            "user":"Licci",
             "email":"licci@gmail.com",
             "password":"liccu",
             "stato":"on"
@@ -48,7 +51,7 @@ var print = new Vue({
             "user":"Lorenzo",
             "email":"lorenzo@gmail.com",
             "password":"lore",
-            "stato":"off"
+            "stato":"on"
         },
         {
             "index":4,
@@ -62,7 +65,7 @@ var print = new Vue({
             "user":"Federico",
             "email":"federico@gmail.com",
             "password":"federico",
-            "stato":"off"
+            "stato":"on"
         },
         {
             "index":6,
@@ -75,9 +78,42 @@ var print = new Vue({
     },
     methods:{
         remove(index){
-        this.itemsDelete.push(this.items[index]);
-        this.items.splice(index,1);
-        this.notifydelete();
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+              confirmButton: 'btn btn-success',
+              cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+          })
+          swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+      
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire(
+                'Deleted!',
+                'Your data has been deleted.',
+                'success'
+              )
+              this.itemsDelete.push(this.items[index]);
+            this.items.splice(index,1);
+            this.notifydelete();
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Your imaginary file is safe :)',
+                'error'
+              )
+            }
+          })
         },
         restore(index,itemindex){
             this.items.splice(itemindex,0,this.itemsDelete[index]);//cosi facendo l'array tornera alla posizione
@@ -85,8 +121,42 @@ var print = new Vue({
             this.notifyrestore();
         },
         removeDelete(index){
-            this.itemsDelete.splice(index,1);
-            this.notifydelete();
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                  confirmButton: 'btn btn-success',
+                  cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+              }) 
+              swalWithBootstrapButtons.fire({
+                title: 'Are you sure?',
+                input: 'text',
+                text:"I dati verrano eliminati definitivamente",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel!',
+                reverseButtons: true
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  swalWithBootstrapButtons.fire(
+                    'Deleted!',
+                    'Your data has been deleted.',
+                    'success'  
+                  )
+                 this.itemsDelete.splice(index,1);
+                    this.notifydelete();
+                } else if (
+                  /* Read more about handling dismissals below */
+                  result.dismiss === Swal.DismissReason.cancel
+                ) {
+                  swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                  )
+                }
+              })
         },
         removeall(){
             this.itemsDelete=[];
@@ -108,7 +178,6 @@ var print = new Vue({
             $(".windowsNotification").addClass("notification-animation bg_color_red");
             setTimeout(function(){ $(".windowsNotification").removeClass("notification-animation"); }, 2000);
         },
-
         notifyrestore(){
             $("#notify").text("");
             $(".windowsNotification").removeClass("notification-animation bg_color_red");
@@ -117,19 +186,22 @@ var print = new Vue({
             setTimeout(function(){ $(".windowsNotification").removeClass("notification-animation"); }, 2000);
         },
          filtered() { 
-           this.itemsFilter="asda";
+             this.message=this.message.charAt(0).toUpperCase()+ this.message.slice(1).toLowerCase()
             if(this.message.length>=1){
                 this.itemsFilter=this.items.filter((element)=>{  
                     return element.user.includes(this.message);
-                    }); 
-                    
-                    console.log(this.itemsFilter);     
-            }
-            else{
-                
+                    });   
+                    console.log(this.itemsFilter);      
+                }
+            else{  
                this.notifyempity()
             }
-
+        },
+        removeFilter(index,itemindex){
+            this.itemsDelete.push(this.itemsFilter[index]);
+            this.itemsFilter.splice(index,1);
+            this.items.splice(itemindex,1);
+            this.notifydelete();
         },
         addItems(){
             if((this.id=="")||(this.nome=="")||(this.email=="")||(this.password=="")||(this.stato=="")){
@@ -141,10 +213,24 @@ var print = new Vue({
                 "user":this.nome,
                 "email":this.email,
                 "password":this.password,
-                "stato":this.stato})
+                "stato":this.stato});
+                this.id="";
+                this.nome="";
+                this.email="";
+                this.password="";
+                this.stato="";
+                Swal.fire(
+                    'Aggiunto Con Successo!',
+                    "",
+                    'success'
+                  )
             }
-           
+          
+        },
+        changeItems(){
+            alert("Ciao")
         }
+     
      
 
     }
